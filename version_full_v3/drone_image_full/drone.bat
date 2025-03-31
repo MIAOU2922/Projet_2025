202b8db4139dc108020c6561ba560a0b9636ab9d
MIAOU <julien.beal2@gmail.com>
2025-03-31 11:24:20 +0200

    patch commandes de start

--- version_full_v3/drone_image_full/drone.bat
+++ version_full_v3/drone_image_full/drone.bat
@@ -1,3 +1,3 @@
 javac -cp "lib/opencv-4100.jar" -d "bin" src/_start/*.java src/thread/*.java src/util/*.java src/main/*.java src/*.java
 
-java -Djava.library.path="lib" -cp "lib/opencv-4100.jar;bin" _start_drone
+java -Djava.library.path="lib" -cp "lib/opencv-4100.jar;bin" _start._start_drone
